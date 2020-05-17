1a1615a1771f56009716d475c43995501308a6d0
Justin Suderman <jsuderman1@gmail.com>
2020-05-16 23:20:33 -0400

    added files

---
+++ game.js
@@ -0,0 +1,7 @@
+var questions = document.getElementById("question");
+var choices = document.getElementsByClassName("choice-text")
+console.log(choices);
+var timer = document.getElementsById("timer")
+
+
+
