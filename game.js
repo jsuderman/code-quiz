49211b4f08e4dcadea7156e2b182220538f0ce10
Justin Suderman <jsuderman1@gmail.com>
2020-08-29 20:29:16 -0400

    added quiz questions and timer

--- game.js
+++ game.js
@@ -1,7 +1,103 @@
-var questions = document.getElementById("question");
-var choices = document.getElementsByClassName("choice-text")
-console.log(choices);
-var timer = document.getElementsById("timer")
+// variables to manipulate the document
+var startButton = document.querySelector("#start-btn");
+var questionButton = document.querySelector("#question");
+var answerButton = document.querySelector("#answer-buttons");
+var timerButton = document.querySelector("#timer");
+var timerSeconds = document.querySelector("#seconds");
+var questionContainer = document.querySelector("#question-container");
+
+var startingSecond = 60;
+let time = startingSecond * 60;
+
+
+setInterval(updateCounter, 1000);
+
+startButton.addEventListener("click", startGame)
 
 
 
+
+
+
+
+var questions = [
+    {
+       question: "Inside which HTML element do we put the JavaScript?",
+       answers: [
+           { text: "<js>", correct: false },
+           { text: "<script>", correct: true },
+           { text: "scripting", correct: false },
+           { text: "<javascript>", correct: false }
+       ]
+    },
+    {
+        question: "Inside which HTML element do we put the JavaScript?",
+       answers: [
+           { text: "<js>", correct: false },
+           { text: "<script>", correct: true },
+           { text: "scripting", correct: false },
+           { text: "<javascript>", correct: false }
+       ]
+    },
+    {
+        question: "Inside which HTML element do we put the JavaScript?",
+       answers: [
+           { text: "<js>", correct: false },
+           { text: "<script>", correct: true },
+           { text: "scripting", correct: false },
+           { text: "<javascript>", correct: false }
+       ]
+    },
+    {
+        question: "Inside which HTML element do we put the JavaScript?",
+       answers: [
+           { text: "<js>", correct: false },
+           { text: "<script>", correct: true },
+           { text: "scripting", correct: false },
+           { text: "<javascript>", correct: false }
+       ]
+    },
+    {
+        question: "Inside which HTML element do we put the JavaScript?",
+       answers: [
+           { text: "<js>", correct: false },
+           { text: "<script>", correct: true },
+           { text: "scripting", correct: false },
+           { text: "<javascript>", correct: false }
+       ]
+    },
+
+]
+
+
+
+
+
+
+
+
+
+function startGame() {
+    console.log("game started")
+    startButton.classList.add("hide")
+    questionContainer.classList.remove("hide")
+    
+    
+}
+
+function updateCounter() {
+    // var minutes = Math.floor(time / 60);
+    let seconds = time % 60;
+
+    timerSeconds.innerHTML = seconds
+    time--;
+
+    if(time <= 0) {
+        clearInterval(time = 0)
+    }
+}
+
+function nextQuestion() {
+
+
+}
